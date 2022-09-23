3afb7293991178f2fd807a9bc4a988d8a61d192f
louaty <74518787+louaty@users.noreply.github.com>
2022-09-23 11:02:14 +0200

    Update PoCColdRoom.ino
    Clean code

--- PoCColdRoom.ino
+++ PoCColdRoom.ino
@@ -12,9 +12,9 @@
 WiFiManager manager;
 const char *mqtt_server = "broker.hivemq.com";
 void callback(char* topic, byte* message, unsigned int length);
-const char *ssid = "";    //hotspot
-const char *password = "";    //password
-
+const char *ssid = "iPhone";
+const char *password = "1234567890";
+//BjGA-aG91ZGEgbG91YXR5 nh7653!!!
 //Wifi Client
 WiFiClient espClient;
 PubSubClient client(espClient);
@@ -24,7 +24,6 @@ const int pin_temp = 39;
 const int pin_button = 36;
 RTC_DATA_ATTR boolean BoutonAppuye = 1;
 RTC_DATA_ATTR float affiche_temp;
-RTC_DATA_ATTR int count = 0;
 
 
 LiquidCrystal_I2C lcd(0x27, 16, 2); // set the LCD address to 0x27 for a 16 chars and 2 line display
@@ -39,8 +38,6 @@ void setup() {
   Serial.println("Setup");
   // initialize the lcd
   lcd.init();
-  Serial.println("Setup");
-
   //Wifi
   WiFi.begin(ssid, password);
   while (WiFi.status() != WL_CONNECTED) {
@@ -63,8 +60,6 @@ void setup() {
   esp_sleep_enable_ext0_wakeup(GPIO_NUM_36, HIGH);
 
   esp_sleep_enable_timer_wakeup(10000000);
-
-
 }
 
 // the loop function runs over and over again forever
@@ -74,7 +69,6 @@ void loop() {
     reconnect();
   }
   client.loop();
-  Serial.println(count);
   boolean state_button = digitalRead(pin_button);
   //calcule de la température
   int value = adc1_get_raw(ADC1_CHANNEL_3);
@@ -90,8 +84,7 @@ void loop() {
   client.publish("IoT_Cours", message.c_str());
   delay(1000);
   Serial.println(message);
-  Serial.println(BoutonAppuye);
-
+  
   if (BoutonAppuye == 0) {
     Serial.println("Bouton appuyé");
     lcd.init();                      // initialize the lcd
@@ -105,7 +98,6 @@ void loop() {
     BoutonAppuye = 1;
   }
   if (BoutonAppuye == 1) {
-
     Serial.println("Bouton fin");
     lcd.setCursor(0, 0);
     lcd.clear();
